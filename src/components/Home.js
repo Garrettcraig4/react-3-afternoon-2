9b03b69684d1925f157e82cb247cf7fa2e05450f
Garrett <Garrettcrg@gmail.com>
2018-01-30 17:13:00 -0600

    Finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,54 @@
-import React, { Component } from 'react';
-import Hero from './subcomponents/Hero';
-import BlogThumb from './subcomponents/BlogThumb';
+import React, { Component } from "react";
+import Hero from "./subcomponents/Hero";
+import BlogThumb from "./subcomponents/BlogThumb";
+import axios from "axios";
+import index from "../../../mini/react-3-mini/node_modules/axios";
+// import axios done
 
-// import axios
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      featured: "",
+      index: 0,
+      posts: [
+        { title: "Loading...", image: "https://unsplash.it/900/400/?random" }
+      ]
+    };
+  }
 
-class Home extends Component{
-    constructor(){
-        super();
-        this.state = {
-            featured: '',
-            index: 0,
-            posts: [{title: "Loading...",image: 'https://unsplash.it/900/400/?random'}]
-        }
-    }
+  // insert componentWillMount:
+  componentWillMount() {
+    axios
+      .get("/api/featured")
+      .then(results => {
+        this.setState({
+          featured: results.data,
+          index: ~~(Math.random() * results.data.length) + 0,
+          posts: results.data
+        });
+      })
+      .catch(console.log);
+  }
 
-    // insert componentWillMount:
-    
+  render() {
+    // map over your recommended blogs here, replace null.
 
-    render(){
-        // map over your recommended blogs here, replace null.
-        const posts = null
+    const posts = this.state.posts.map((c, i) => (
+      <BlogThumb key={i} blog={c} />
+    ));
 
-        return(
-            <div className="content" >
-                <Hero blog={this.state.posts[this.state.index]} />
-                <hr/>
-                <div className="blog-grid">
-                    {/* put your mapped blogs here */}
-                    {posts}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="content">
+        <Hero blog={this.state.posts[this.state.index]} />
+        <hr />
+        <div className="blog-grid">
+          {/* put your mapped blogs here */}
+          {posts}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
